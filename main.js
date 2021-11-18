4375940293d3d49948818c1098ec90d6f794d744
Daramola femi <dfmopeoluwa@gmail.com>
2021-11-18 18:41:43 +0100

    my first extension

---
+++ main.js
@@ -0,0 +1,4 @@
+let content = document.getElementById('contents')
+let rand = Math.round(Math.random()*100)
+content.innerHTML=`<div><h1 class='text'>Hey yo. Kindly leave here right now.<h1> <img class='image' src='https://unsplash.it/1200/800?image=${rand}' </div>`
+
